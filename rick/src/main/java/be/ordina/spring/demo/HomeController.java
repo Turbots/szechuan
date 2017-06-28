f27018751db5228ca569f1740787a3671dd32d6e
Dieter Hubau <dhubau@gmail.com>
2017-06-28 15:31:17 +0200

    Added post endpoint to start finding sauce

--- rick/src/main/java/be/ordina/spring/demo/HomeController.java
+++ rick/src/main/java/be/ordina/spring/demo/HomeController.java
@@ -1,6 +1,8 @@
 package be.ordina.spring.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HomeController {
 
+	private final SzechuanFinder szechuanFinder;
+
+	@Autowired
+	public HomeController(SzechuanFinder szechuanFinder) {
+		this.szechuanFinder = szechuanFinder;
+	}
+
 	@GetMapping
 	public String homepage() {
-		return "WUB-A-DUB-A-DUB-DUB!";
+		return "WUB-A-DUB-A-DUB-DUB! LETS GET THAT SAUCE!";
+	}
+
+	@PostMapping
+	public String findTheSauce() throws InterruptedException {
+		this.szechuanFinder.findThatSauce();
+
+		return "YOU GOT IT!";
 	}
 }
--- rick/src/main/java/be/ordina/spring/demo/SzechuanFinder.java
+++ rick/src/main/java/be/ordina/spring/demo/SzechuanFinder.java
@@ -2,7 +2,6 @@ package be.ordina.spring.demo;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.integration.support.MessageBuilder;
@@ -10,12 +9,14 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class SzechuanFinder implements CommandLineRunner {
+public class SzechuanFinder {
 
 	private final MeeseeksResource meeseeksResource;
 	private final OutputChannels outputChannels;
 
 	private static final int minimumRequestIntervalInMillis = 50;
+
+	private static boolean SEARCHING = false;
 
 	@Autowired
 	public SzechuanFinder(MeeseeksResource meeseeksResource, OutputChannels outputChannels) {
@@ -23,30 +24,33 @@ public class SzechuanFinder implements CommandLineRunner {
 		this.outputChannels = outputChannels;
 	}
 
-	@Override
-	public void run(String... strings) throws Exception {
-		WishResult result = WishResult.NOT_GRANTED;
-		Wish wish = new Wish("1989 MCNUGGET SZECHUAN DIPPING SAUCE");
-		int requestIntervalInMillis = 5000;
+	public void findThatSauce() throws InterruptedException {
+		if (!SEARCHING) {
+			SzechuanFinder.SEARCHING = true;
+			WishResult result = WishResult.NOT_GRANTED;
+			Wish wish = new Wish("1989 MCNUGGET SZECHUAN DIPPING SAUCE");
+			int requestIntervalInMillis = 5000;
 
-		while (result == WishResult.NOT_GRANTED) {
-			outputChannels.rick()
-				.send(MessageBuilder.withPayload("Mr Meeseeks, get me that McNugget Szechuan Dipping Sauce").build());
-			ResponseEntity<WishResult> response = this.meeseeksResource.makeWish(wish);
+			while (result == WishResult.NOT_GRANTED) {
+				outputChannels.rick()
+					.send(
+						MessageBuilder.withPayload("Mr Meeseeks, get me that McNugget Szechuan Dipping Sauce").build());
+				ResponseEntity<WishResult> response = this.meeseeksResource.makeWish(wish);
 
-			if (response.getStatusCode() == HttpStatus.OK) {
-				result = response.getBody();
-				log.info("Mr Meeseeks came back with [{}]", result);
-			} else {
-				log.error("Mr Meeseeks seems to be malfunctioning! [HTTP " + response.getStatusCode() + "]");
+				if (response.getStatusCode() == HttpStatus.OK) {
+					result = response.getBody();
+					log.info("Mr Meeseeks came back with [{}]", result);
+				} else {
+					log.error("Mr Meeseeks seems to be malfunctioning! [HTTP " + response.getStatusCode() + "]");
+				}
+
+				Thread.sleep(requestIntervalInMillis);
+				requestIntervalInMillis = Math.max(minimumRequestIntervalInMillis, requestIntervalInMillis - 100);
 			}
 
-			Thread.sleep(requestIntervalInMillis);
-			requestIntervalInMillis = Math.max(minimumRequestIntervalInMillis, requestIntervalInMillis - 100);
+			log.info("Found it!");
+			outputChannels.rick()
+				.send(MessageBuilder.withPayload("WUB-A-DUB-A-DUB-DUB, I FINALLY GOT MY SAUCE!").build());
 		}
-
-		log.info("Found it!");
-		outputChannels.rick()
-			.send(MessageBuilder.withPayload("WUB-A-DUB-A-DUB-DUB, I FINALLY GOT MY SAUCE!").build());
 	}
 }
