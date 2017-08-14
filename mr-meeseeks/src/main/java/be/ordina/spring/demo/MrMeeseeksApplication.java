2f68849e03b5f875bca3236c368491830fafd89d
Dieter Hubau <dhubau@gmail.com>
2017-08-14 23:08:43 +0200

    WIP changing to all messaging

--- mr-meeseeks/src/main/java/be/ordina/spring/demo/MrMeeseeksApplication.java
+++ mr-meeseeks/src/main/java/be/ordina/spring/demo/MrMeeseeksApplication.java
@@ -7,14 +7,13 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @Slf4j
 @EnableScheduling
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
-@EnableBinding(OutputChannels.class)
+@EnableBinding({ InputChannels.class, OutputChannels.class })
 public class MrMeeseeksApplication {
 
 	public static void main(String[] args) {
