439257c8203c314b710cac9bdd785a1c2d2c3f1c
Dieter Hubau <Dieter.Hubau@nike.com>
2017-08-18 15:07:00 +0200

    First version with Vue.js

--- morty/frontend/src/main.js
+++ morty/frontend/src/main.js
@@ -1,9 +1,7 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true
 
 /* eslint-disable no-new */
 new Vue({
